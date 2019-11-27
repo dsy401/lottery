eb7023b92885ab252b8343f249935fa5f65fab2d
Oliver <oliverdeng1005@hotmail.com>
2019-11-27 15:46:57 +1300

    update

--- server/app.js
+++ server/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const fs = require('fs');
-const csv = require('csv-parser');
-
+const bodyParser = require('body-parser')
 
 const app = express();
+
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+)
+
+app.use(bodyParser.json())
+
 
 app.all('*',function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -11,24 +19,37 @@ app.all('*',function (req, res, next) {
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
 
     if (req.method == 'OPTIONS') {
-        res.send(200);
+        res.sendStatus(200)
     }
     else {
         next();
     }
 });
 
-app.use('/',())
+app.use('/add',(req,res)=>{
+    fs.readFile('./data/people.json', (err, data) => {
+        if (err) throw err;
+        let people = JSON.parse(data);
+
+        const name = req.body.Name;
+        const phone = req.body.Phone
+        const Postdata = {"Name": name,"Phone":phone}
+        people.push(Postdata)
+
+        fs.writeFile("./data/people.json",JSON.stringify(people),err=>{
+            if (err) throw err;
+            res.json({msg:"success"})
+        })
+    });
+});
+
 
 app.use('/',(req,res)=>{
-    let result = []
-    fs.createReadStream('./data/people.csv')
-        .pipe(csv())
-        .on('data',(row)=>{
-            result.push(row)
-        }).on("end",()=>{
-            res.json(result)
-    })
+    fs.readFile('./data/people.json', (err, data) => {
+        if (err) throw err;
+        let people = JSON.parse(data);
+        res.json(people)
+    });
 });
 
 
